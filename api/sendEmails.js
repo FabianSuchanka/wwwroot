b186dc6b695d999378f600fcd3864723eb4f26e2
FabianSuchanka <120109205+FabianSuchanka@users.noreply.github.com>
2025-10-19 00:24:47 +0200

    Update sendEmails.js

--- api/sendEmails.js
+++ api/sendEmails.js
@@ -6,6 +6,7 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+    console.log("Handler volán");  // Tohle se musí objevit v logu!
   if (req.method !== 'POST') return res.status(405).send('Method not allowed');
 
   let body = '';
@@ -42,3 +43,4 @@ export default async function handler(req, res) {
     }
   });
 }
+
